7d5f1b3a620d14a24b613a2dac155d776cee2f62
Aaron Chafetz <achafetz@usaid.gov>
2019-03-15 17:08:45 +0300

    function for adding groupings

---
+++ R/add_groupings.R
@@ -0,0 +1,17 @@
+#' Add PSNU and Age groupsings
+#'
+#' @param df site tool data frame to apply to
+#'
+#' @importFrom magrittr %>%
+#' @export
+
+add_groupings <- function(df){
+
+  df <- df %>%
+    tidyr::separate(Site, c("psnu", "sitename"), sep = " > ", remove = FALSE) %>% #separate out psnu from site for grouping
+    dplyr::mutate(psnu = stringr::str_remove(psnu, " \\(.*$")) %>% #clean up psnu name (where undistributed)
+    tibble::add_column(AgeCoarse = ifelse(df$Age %in% peds, "<15", "15+"), .after = "Age")
+
+  return(df)
+}
+
