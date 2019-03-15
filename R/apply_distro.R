2ea30c48d03433f6772663c51bee5c363e0d1f59
Aaron <achafetz@usaid.gov>
2019-03-15 22:25:17 +0300

    function to apply distribution

---
+++ R/apply_distro.R
@@ -0,0 +1,24 @@
+#' Apply Distribution to
+#'
+#' @param df dataframe to apply distributions on
+#' @param df_distro distribution dataframe created with `gen_distro()`
+#' @param ... variables to apply the distribution to
+#'
+#' @importFrom magrittr %>%
+#' @export
+
+apply_distro <- function(df, df_distro, ...){
+
+  indicator_quo <- dplyr::enquos(...)
+
+  #suppressMessages(
+  df_join <- dplyr::left_join(df, df_distro)
+  #)
+  df_join <- df_join %>%
+    dplyr:: group_by(psnu, Mechanism, sitename, AgeCoarse) %>%
+    dplyr::mutate_at(dplyr::vars(!!!indicator_quo), ~ distro * sum(.)) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(-distro)
+
+  return(df_join)
+}
