2e6203534df0b787bb48d0c34c5cc419cb5ae695
Aaron Chafetz <achafetz@usaid.gov>
2019-03-16 08:53:59 +0300

    drop columns with lowercase (ie weren't in original Site file

---
+++ R/match_orig.R
@@ -0,0 +1,12 @@
+#' Match original columns of the Site Tool
+#'
+#' @param df adjusted dataframe
+#'
+#' @export
+
+match_orig <- function(df){
+
+  df <- dplyr::select(df. -dplyr::matches("^[a-z].*", ignore.case = FALSE))
+
+  return(df)
+}
