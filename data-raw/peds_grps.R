47509ebae9be736ab3f7506a033e992943b36bc7
Aaron Chafetz <achafetz@usaid.gov>
2019-03-19 13:31:53 +0300

    update peds groups to include coarse u15 to deal with TX_TB and TB_PREV

--- data-raw/peds_grps.R
+++ data-raw/peds_grps.R
@@ -1,9 +1,9 @@
 #Set peds age groups for grouping purposes
 
-peds <- c("<01", "01-04", "05-09", "10-14")
-peds_o01 <- c("01-04", "05-09", "10-14")
+peds <- c("<01", "01-04", "05-09", "10-14", "<15")
+peds_o01 <- c("01-04", "05-09", "10-14", "<15")
 
-use_data(peds)
-use_data(peds_o01)
+use_data(peds, overwrite = TRUE)
+use_data(peds_o01, overwrite = TRUE)
 
 rm(peds, peds_o01)
